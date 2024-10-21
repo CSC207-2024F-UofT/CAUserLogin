9bc7ab0709fd6ecde25dcf74afb3e866e3aec4ca
Edison Yao <edisonj.yao@mail.utoronto.ca>
2024-10-20 23:00:40 -0400

    Completed Phase 1, Task 2.1

--- src/main/java/app/MainWithInMemory.java
+++ src/main/java/app/MainWithInMemory.java
@@ -53,6 +53,8 @@ public class MainWithInMemory {
         // TODO Task 1.1 in a copy of this file, change this line to use the in-memory DAO.
         final InMemoryUserDataAccessObject userDataAccessObject = new InMemoryUserDataAccessObject();
 
+        // userDataAccessObject.setCurrentUser(user.getName());
+
         final SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loginViewModel,
                                                                   signupViewModel, userDataAccessObject);
         views.add(signupView, signupView.getViewName());
