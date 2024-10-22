6b1c884b7408e9dbbe3039c84da940f24c0e2eb6
purbapat <patuan.purba@mail.utoronto.ca>
2024-10-21 21:53:03 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Set current user.
+     * @param name the username to look up
+     */
+    void setCurrentUser(String name);
 }
