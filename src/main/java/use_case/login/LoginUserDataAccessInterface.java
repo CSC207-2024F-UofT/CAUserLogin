5e3859dfe5aaf0d79260609f59a681c1c9f85f53
tvirat <155598251+tvirat@users.noreply.github.com>
2024-10-25 16:10:49 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current username as given.
+     * @param name the name to be set as the current username.
+     */
+    void setCurrentUser(String name);
 }
