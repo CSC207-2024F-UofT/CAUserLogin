9d334eed2cc361fd55bdda0a940ac86f8476d631
chs- <braddy1217@gmail.com>
2024-10-27 16:24:59 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * setCurrentUser method.
+     * @param name the name.
+     */
+    void setCurrentUser(String name);
 }
