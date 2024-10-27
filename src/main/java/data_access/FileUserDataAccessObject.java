0355955bfcedb761cac0cc09e83b2d0d4906937e
gbr-7 <xi.shen@mail.utoronto.ca>
2024-10-27 15:35:32 -0400

    aass

--- src/main/java/data_access/FileUserDataAccessObject.java
+++ src/main/java/data_access/FileUserDataAccessObject.java
@@ -99,7 +99,7 @@ public class FileUserDataAccessObject implements SignupUserDataAccessInterface,
 
     @Override
     public String getCurrentUser() {
-        return "";
+        return null;
     }
 
     @Override
